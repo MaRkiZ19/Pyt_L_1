5973daaa5a47d021caff5ac9f365e8c390d1ec71
ваше_имя <grigorenko-marko@rambler.ru>
2023-05-21 17:56:30 +0300

    lection 1

---
+++ main.py
@@ -0,0 +1,50 @@
+# print('введите первое число: ')
+# a=input()
+# b=input('введите второе число: ')
+# c='hello'
+
+# print(a, '+', b, '=', a+b)
+
+
+# c = 5.85
+# print(c)
+# c = int(c)
+# print(n)
+
+# c = 5.85
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+
+# print('введите первое число: ')
+# a=int(input())
+# b=int(input('введите второе число: '))
+# c='hello'
+
+# print(a, '+', b, '=', a+b)
+
+# a = 5.0987986
+# b=6.0977
+# print(round(a*b, 3))
+
+# username = input('Введите user name: ')
+# if username == 'Маша':
+#     print('Ура Маше')
+# elif username == 'Марко':
+#     print('Хорош тупить')
+# else:
+#     print('Hi', username)
+
+r = range(1, 100, 5)
+for i in r:
+    print(i)
